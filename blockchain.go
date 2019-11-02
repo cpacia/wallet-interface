306d320dec2b4eb502715a287b7649e9b53eae2b
Chris <chris@ob1.io>
2019-11-01 23:08:14 -0400

    Add string method to BlockID

--- blockchain.go
+++ blockchain.go
@@ -3,8 +3,14 @@ package wallet_interface
 // BlockID represents an ID for a block on the network.
 type BlockID string
 
+// String returns the string representation of the ID.
+func (b *BlockID) String() string {
+	return string(*b)
+}
+
 // BlockchainInfo holds the blockchain's best hash and height.
 type BlockchainInfo struct {
 	BestBlock BlockID
 	Height    uint64
 }
+
